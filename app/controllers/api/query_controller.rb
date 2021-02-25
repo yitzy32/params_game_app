27526b7fc1d9c1906ee6338febf9423ab1a1f359
Yitzy Bratt <yitzybratt32@gmail.com>
2021-02-24 20:15:01 -0500

    prints first name in all caps to server

--- app/controllers/api/query_controller.rb
+++ app/controllers/api/query_controller.rb
@@ -1,5 +1,13 @@
 class Api::QueryController < ApplicationController
   def query_string
+    p params[:name].upcase
+    if params[:name][0] == "a"
+      p "Hey, your name starts with the first letter of the alphabet!"
+    end
     render "name.json.jb"
+  end
+
+  def number_guess
+    render "number.json.jb"
   end
 end
